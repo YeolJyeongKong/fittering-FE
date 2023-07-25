b835f6f096b2706a522c2394561edd525d2a598c
thumbzzero <thumbzzero@gmail.com>
2023-07-25 22:01:47 +0900

    refactor: 마이페이지/화면 여백 관련 코드 리팩토링 및 폴더 구조 변경

--- components/MyPage/MyPageSection.tsx
+++ components/MyPage/MyPageSection.tsx
@@ -1,10 +1,10 @@
 type Props = {
-  sectionName: "최근 본 상품" | "좋아요한 상품";
+  sectionName: '최근 본 상품' | '좋아요한 상품' | '내 맞춤 정보 설정';
 };
 
 export default function MyPageSection({ sectionName }: Props) {
   return (
-    <div className="mx-4 md:mx-10 lg:mx-48 my-8 pb-2 border-b border-nav-black">
+    <div className="my-8 pb-2 border-b border-nav-black">
       <span className="font-bold">{sectionName}</span>
     </div>
   );
